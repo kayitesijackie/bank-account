e60ec549f24f36aec04774be24cbb9133b0f88e2
Kayitesi Jackie <kayitesijacqueline@gmail.com>
2019-01-20 00:56:58 +0200

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,75 +1,3 @@
-// var currentAccount = "";
-
-// $(function(){
-//   $("#form-account").submit(function(event){
-//     event.preventDefault();
-
-//     var userName = $("#name").val();
-//     var IdNumber = $("#id-number").val();
-//     // var email = $("#email").val();
-//     var phoneNumber = $("#phone-number");
-//     var balance = parseFloat($("#balance").val());
-    
-    
-//     if(isNaN(balance))
-//     {
-//       balance = 0;
-//     }
-//     var newAccount = new Account(userName,IdNumber,phoneNumber,balance);
-//     currentAccount = newAccount;
-//     newAccount.output();
-//   });
-
-//   $("#form-balance").submit(function(event){
-//     event.preventDefault();
-//     if(currentAccount != "")
-//     {
-//       var deposit = parseFloat($("#deposit").val());
-//       var withdraw = parseFloat($("#withdraw").val());
-//       if(isNaN(deposit))
-//       {
-//         deposit = 0;
-//       }
-//       if(isNaN(withdraw))
-//       {
-//         withdraw = 0;
-//       }
-//       var temp = deposit-withdraw;
-//       currentAccount.changeBalance(temp);
-//       currentAccount.output();
-//     }
-//   });
-
-// });
-
-// //Create a constructor for object Task
-// function Account(inputUserName,inputIdNumber,inputPhoneNumber,inputBalance){
-//   this.name = inputUserName;
-//   this.id-number= inputIdNumber;
-//   // this.email = inputEmail;
-//   this.phone-number = inputPhoneNumber;
-//   this.balance = inputBalance;
-// }
-
-// Account.prototype.output = function ()
-// {
-//   $(".result").text(formatRWF(this.balance));
-// };
-
-// Account.prototype.changeBalance = function (amount)
-// {
-//   this.balance += amount;
-// }
-
-// function clear(temp)
-// {
-//   $(temp).val("");
-// }
-
-// function formatRWF(tempString)
-// {
-//   return tempString.toLocaleString('en-RW',{style: 'currency', currency: 'RWF'});
-// }
 var currentAccount = "";
 
 $(function(){
@@ -78,13 +6,16 @@ $(function(){
 
     var userName = $("#name").val();
     var IdNumber = $("#id-number").val();
+    var email = $("#email").val();
+    var phoneNumber = $("#phone-number");
     var balance = parseFloat($("#balance").val());
+    
     
     if(isNaN(balance))
     {
       balance = 0;
     }
-    var newAccount = new BankAccount(userName,IdNumber,balance);
+    var newAccount = new Account(userName,IdNumber,phoneNumber,balance);
     currentAccount = newAccount;
     newAccount.output();
   });
@@ -112,18 +43,20 @@ $(function(){
 });
 
 //Create a constructor for object Task
-function BankAccount(inputUserName,inputIdNumber,inputBalance){
+function Account(inputUserName,inputIdNumber,inputEmail,inputPhoneNumber,inputBalance){
   this.name = inputUserName;
   this.id-number= inputIdNumber;
+  this.email = inputEmail;
+  this.phone-number = inputPhoneNumber;
   this.balance = inputBalance;
 }
 
-BankAccount.prototype.output = function ()
+Account.prototype.output = function ()
 {
   $(".result").text(formatRWF(this.balance));
 };
 
-BankAccount.prototype.changeBalance = function (amount)
+Account.prototype.changeBalance = function (amount)
 {
   this.balance += amount;
 }
